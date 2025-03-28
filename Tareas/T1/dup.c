1f2b7c9402c173687c88de21b69093f03ba52b25
Juan Pablo Bustamante <juan.bustamante.f@ug.uchile.cl>
2025-03-28 14:03:06 -0300

    28/03 14:03

--- Tareas/T1/dup.c
+++ Tareas/T1/dup.c
@@ -3,11 +3,11 @@
 unsigned long long dupMasLargo(unsigned long long x){
     unsigned long long mask1 = ~0ULL >> 32;
     unsigned long long mask2 = ~0ULL << 32;
-    for(int i = 32; i > 0; i --) {
+    for(int i = 32; i > 0; i--) {
         unsigned long long sufijo = x & mask1;
         unsigned long long prefijo = x & mask2;
         prefijo = prefijo >> i;
-       if(sufijo == prefijo){
+        if(sufijo == prefijo){
             return i;
         } else {
             mask1 = mask1 >> 1;
@@ -17,8 +17,8 @@ unsigned long long dupMasLargo(unsigned long long x){
     return 0;
 }
 
-int main() {
-    unsigned long long num = 0b01;
+/*int main() {
+    unsigned long long num = 0b0;
     printf("%d", dupMasLargo(num));
     return 0;
-}
+}*/
