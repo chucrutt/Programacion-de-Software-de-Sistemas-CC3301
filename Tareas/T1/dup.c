b559d460b86fe28ab0ec8c6f2b77afbfd5c0b7ce
Juan Pablo Bustamante <juan.bustamante.f@ug.uchile.cl>
2025-03-28 13:45:41 -0300

    28/03 13:45

--- Tareas/T1/dup.c
+++ Tareas/T1/dup.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
-void imprimirBits(unsigned long long num) {
-    int i;
-    int numBits = sizeof(num) * 8;
-
-    for (i = numBits - 1; i >= 0; i--) {
-        printf("%d", (num >> i) & 1);
-        if (i % 4 == 0) {
-            printf(" ");
-        }
-    }
-    printf("\n");
-}
-
 unsigned long long dupMasLargo(unsigned long long x){
     unsigned long long mask1 = ~0ULL >> 32;
     unsigned long long mask2 = ~0ULL << 32;
@@ -21,17 +8,17 @@ unsigned long long dupMasLargo(unsigned long long x){
         unsigned long long prefijo = x & mask2;
         prefijo = prefijo >> i;
        if(sufijo == prefijo){
-            imprimirBits(prefijo);
-            return 0;
+            return i;
         } else {
             mask1 = mask1 >> 1;
-            mask2 = mask1 << i-1;
+            mask2 = mask1 << (i-1);
         }
     }
+    return 0;
 }
 
 int main() {
-    unsigned long long num = 0b101100100100100100100;
-    dupMasLargo(num);
+    unsigned long long num = 0b01;
+    printf("%d", dupMasLargo(num));
     return 0;
 }
