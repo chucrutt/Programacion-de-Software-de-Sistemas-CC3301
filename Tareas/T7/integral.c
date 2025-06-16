37063f29e5cbc12bea43ba2005280e50f8155795
Juan Pablo Bustamante <juan.bustamante.f@ug.uchile.cl>
2025-06-16 15:04:35 -0400

    comenzando tarea 7

---
+++ Tareas/T7/integral.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "integral.h"
+
+double integral_par(Funcion f, void *ptr, double xi, double xf, int n, int p) {
+  ... completar ...
+  return ...;
+}
