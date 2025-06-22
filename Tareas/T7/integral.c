88a9896ab793446db497775ebf713a6428e1a9f8
Juan Pablo Bustamante <juan.bustamante.f@ug.uchile.cl>
2025-06-22 18:47:09 -0400

    Tarea 7 terminada

--- Tareas/T7/integral.c
+++ Tareas/T7/integral.c
@@ -8,6 +8,43 @@
 #include "integral.h"
 
 double integral_par(Funcion f, void *ptr, double xi, double xf, int n, int p) {
-  ... completar ...
-  return ...;
+    int fd[p][2];
+    double h = (xf - xi) / p;
+    int trapecios_por_proceso = n / p;
+
+    for (int i = 0; i < p; i++) {
+        pipe(fd[i]);
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            close(fd[i][0]);
+
+            double sub_xi = xi + i * h;
+            double sub_xf = sub_xi + h;
+
+            int trapecios = (i == p - 1) ? (n - trapecios_por_proceso * (p - 1)) : trapecios_por_proceso;
+
+            double resultado = integral(f, ptr, sub_xi, sub_xf, trapecios);
+            write(fd[i][1], &resultado, sizeof(double));
+
+            close(fd[i][1]);
+            exit(0);
+        }
+
+        close(fd[i][1]);
+    }
+
+    double resultado_total = 0.0;
+    for (int i = 0; i < p; i++) {
+        double parcial = 0.0;
+        read(fd[i][0], &parcial, sizeof(double));
+        resultado_total += parcial;
+        close(fd[i][0]);
+    }
+
+    for (int i = 0; i < p; i++) {
+        wait(NULL);
+    }
+
+    return resultado_total;
 }
